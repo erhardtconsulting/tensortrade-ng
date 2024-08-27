48adee6fec562a18d6432a5ac71f135e0bb2ed3b
Simon Erhardt <hello@rootlogin.ch>
2024-08-27 16:11:02 +0200

    chore: fix imports for tests

--- src/tensortrade/__init__.py
+++ src/tensortrade/__init__.py
@@ -1,5 +1,19 @@
-from . import core
-from . import feed
+# Copyright 2024 The TensorTrade-NG Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+from tensortrade import core
+from tensortrade import env
+from tensortrade import feed
 from tensortrade.oms import (
     orders,
     wallets,
@@ -7,6 +21,6 @@ from tensortrade.oms import (
     exchanges,
     services
 )
-from . import env
+from tensortrade import pipeline
 
-from .__about__ import __version__
+from tensortrade.__about__ import __version__
